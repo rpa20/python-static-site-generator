818b3c79827a941350c769e4db71660e4d5fc13b
rpa20 <ryan_aboussafy@outlook.com>
2022-12-05 12:53:32 -0500

    Completed module

--- ssg.py
+++ ssg.py
@@ -4,10 +4,8 @@ from ssg.site import Site
 
 
 def main(source="content", dest="dist"):
-    config = {
-        "source": source,
-        "dest": dest
-    }
+    config = {"source": source, "dest": dest}
+
     Site(**config).build()
 
 
