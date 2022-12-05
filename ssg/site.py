ba16b097557889fff0df1260deda4260074605f4
rpa20 <ryan_aboussafy@outlook.com>
2022-12-05 12:49:38 -0500

    Completed module

--- ssg/site.py
+++ ssg/site.py
@@ -2,7 +2,7 @@ from pathlib import Path
 
 
 class Site:
-    def __int__(self, source, dest,):
+    def __int__(self, source, dest):
         self.source = Path(source)
         self.dest = Path(dest)
 
